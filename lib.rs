bd3b0ddfb56da135167b28a2bdc05dadccce04b3
mmacedoeu <contato@mmacedo.eu.org>
2021-06-01 23:10:36 -0300

    ADD register works test

--- lib.rs
+++ lib.rs
@@ -18,6 +18,7 @@ mod myns {
     /// Add new fields to the below struct in order
     /// to add new static storage fields to your contract.
     #[ink(storage)]
+    #[derive(Default)]
     pub struct Myns {
         /// Stores a name with it's owner
         registry: StorageHashMap<Hash, (AccountId, BlockNumber, u32)>,
@@ -42,16 +43,10 @@ mod myns {
     }
 
     impl Myns {
-        /// Constructor that initializes the `bool` value to `false`.
-        ///
         /// Constructors can delegate to other constructors.
         #[ink(constructor)]
-        pub fn default() -> Self {
-            Self {
-                registry: Default::default(),
-                commit_name: Default::default(),
-                commit: Default::default(),
-            }
+        pub fn new() -> Self {
+            Default::default()
         }
 
         /// Simply returns the current Hash value of our `name`.
@@ -92,7 +87,7 @@ mod myns {
             }
         }
 
-        #[ink(message)]
+        #[ink(message, selector = "0x1EECBEEF")]
         pub fn make_commitment(&self, name: String, owner: AccountId, secret: u32) -> Hash {
             let mut out = [0; 32];
             let mut pimage: Vec<u8> = Vec::new();
@@ -132,7 +127,7 @@ mod myns {
             } else {
                 None
             }
-        }        
+        }
 
         fn not_expired_commit(&self, b: &BlockNumber) -> bool {
             let t = self.env().block_number() - *b;
@@ -156,7 +151,9 @@ mod myns {
         fn commit_register(&self, commitment: &Hash) {
             if let Some(ref b) = self.commit.get(commitment) {
                 let not_expired_commit = self.not_expired_commit(b);
-                ink_env::debug_println(format!("commit not expired: {}", not_expired_commit).as_str());
+                ink_env::debug_println(
+                    format!("commit not expired: {}", not_expired_commit).as_str(),
+                );
                 assert!(not_expired_commit, "commit expired");
             } else {
                 ink_env::debug_println("no commit");
@@ -175,6 +172,7 @@ mod myns {
             );
             self.commit_check(&commitment);
             self.commit.insert(commitment, self.env().block_number());
+            ink_env::debug_println("commited");
         }
 
         fn unlock_balance(&mut self, name_hash: Hash) {
@@ -246,7 +244,7 @@ mod myns {
             self.unlock_balance(name);
             self.env().emit_event(Unregister { name });
         }
-        
+
         pub fn is_owner(&self, name: Hash) -> bool {
             if let Some((ref o, _b, _d)) = self.registry.get(&name) {
                 self.env().caller() == *o
@@ -264,16 +262,30 @@ mod myns {
                 panic!("not the owner");
             }
         }
+
+        // #[ink(message, payable, selector = "0xCAFEBEEF")]
+        // pub fn renew(&mut self, name: String, duration: u32) {
+        //     let hash_name = self.get_hash_name(name);
+        //     if let Some((ref o, ref b, ref d)) = self.registry.get(&hash_name) {
+        //         let t = self.env().block_number() - *b;
+        //         ink_env::debug_println(format!("elapsed: {} == {}", t, *d).as_str());
+
+        //     } else {
+        //         ink_env::debug_println("not registered");
+        //         panic!("not registered");
+        //     }
+        // }
     }
 
     /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
     /// module and test functions are marked with a `#[test]` attribute.
     /// The below code is technically just normal Rust code.
+    #[cfg(not(feature = "ink-experimental-engine"))]
     #[cfg(test)]
     mod tests {
         /// Imports all the definitions from the outer scope so we can use them here.
         use super::*;
-
+        use ink_env::{call, test};
         /// Imports `ink_lang` so we can use `#[ink::test]`.
         use ink_lang as ink;
 
@@ -286,9 +298,96 @@ mod myns {
 
         /// We test a simple use case of our contract.
         #[ink::test]
-        fn it_works() {
+        fn register_works() {
+            // given
+            let contract_balance = 100;
+            let accounts = default_accounts();
+            set_sender(accounts.alice);
+            set_balance(contract_id(), contract_balance);
             let mut myns = Myns::default();
-            // assert_eq!(myns.get(), false);
+
+            // when
+            set_sender(accounts.eve);
+            set_balance(accounts.eve, 100);
+
+            assert!(myns.available("myname".to_owned()));
+            let commitment = myns.make_commitment("myname".to_owned(), accounts.eve, 1);
+            commit(&accounts.eve, &commitment);
+            myns.commit(commitment);
+            assert!(myns.available("myname".to_owned()));
+            register("myname".to_owned(), &accounts.eve, 100, 1);
+            myns.register("myname".to_owned(), accounts.eve, 100, 1);
+            assert!(!myns.available("myname".to_owned()));
+        }
+
+        fn contract_id() -> AccountId {
+            ink_env::test::get_current_contract_account_id::<ink_env::DefaultEnvironment>()
+                .expect("Cannot get contract id")
+        }
+
+        fn set_sender(sender: AccountId) {
+            let callee =
+                ink_env::account_id::<ink_env::DefaultEnvironment>().unwrap_or([0x0; 32].into());
+            test::push_execution_context::<Environment>(
+                sender,
+                callee,
+                1000000,
+                1000000,
+                test::CallData::new(call::Selector::new([0x00; 4])), // dummy
+            );
+        }
+
+        fn default_accounts() -> ink_env::test::DefaultAccounts<ink_env::DefaultEnvironment> {
+            ink_env::test::default_accounts::<ink_env::DefaultEnvironment>()
+                .expect("Off-chain environment should have been initialized already")
+        }
+
+        fn set_balance(account_id: AccountId, balance: Balance) {
+            ink_env::test::set_account_balance::<ink_env::DefaultEnvironment>(account_id, balance)
+                .expect("Cannot set account balance");
+        }
+
+        fn commit(account: &AccountId, commitment: &Hash) {
+            // 0xDEADBEEF
+            let mut data = ink_env::test::CallData::new(ink_env::call::Selector::new([
+                0xDE, 0xAD, 0xBE, 0xEF,
+            ]));
+            data.push_arg(commitment);
+            let mock_transferred_balance = 100;
+
+            // Push the new execution context which sets Eve as caller and
+            // the `mock_transferred_balance` as the value which the contract
+            // will see as transferred to it.
+            ink_env::test::push_execution_context::<ink_env::DefaultEnvironment>(
+                *account,
+                contract_id(),
+                1000000,
+                mock_transferred_balance,
+                data,
+            );
+        }
+
+        fn register(name: String, from: &AccountId, duration: u32, secret: u32) {
+            // 0xCAFEBABE
+            let mut data = ink_env::test::CallData::new(ink_env::call::Selector::new([
+                0xCA, 0xFE, 0xBA, 0xBE,
+            ]));
+            data.push_arg(&name);
+            data.push_arg(from);
+            data.push_arg(&duration);
+            data.push_arg(&secret);
+            let mock_transferred_balance = 600010;
+
+            // Push the new execution context which sets 'from' as caller and
+            // the `mock_transferred_balance` as the value which the contract
+            // will see as transferred to it.
+            ink_env::test::push_execution_context::<ink_env::DefaultEnvironment>(
+                *from,
+                contract_id(),
+                1000000,
+                mock_transferred_balance,
+                data,
+            );
         }
     }
 }
